3d524077895c0272322a016eb1285a6b287b89b6
Gild56 <gild56gmd@gmail.com>
2025-07-04 20:06:18 +0200

    initial commit

---
+++ challenges_list.py
@@ -0,0 +1,19 @@
+challenges_list_top = [
+    ("12qwe2d1", "108396972", "A remake of a wave challenge \"12qwe2d\" from the recent tab.", "hwpEIjr2yK4"),
+    ("Slaughterhouse Spam", "116995082", "Repeated 9 times spam from Slaughterhouse but without decoration. I accidentally beat it once lol xD", ""),
+    ("RubRub", "114906934", "A random layout with a random name. It's also known as \"MysticNull Challenge\" or \"Litox Challenge\"", "lvV6n0OnDJ8"),
+    ("Spam VIII", "119573592", "Very long blue orbs ball spam.", ""),
+    ("Wave Challenge Final", "116005641", "The layout of a random wave challenge.", "12kSrHiKKy8"),
+    ("LegendOfConsistensy", "92834008", "2 players mode consistensy challenge.", "89DMw4Mizag", {"Uranium": "DzTbL22toR8"}),
+    ("Pushing On", "106145402", "Small wave consistensy challenge, the structure is repeated 4 times", "Fk_E80P2xo0", {"Uranium": "DzTbL22toR8"}),
+    ("RI Wave x2", "92957129", "Rainbow had an \"impossible\" version and this is the wave from that version but two times in a row.", "4qaz8HmjEiA", {"Uranium": "DzTbL22toR8"}),
+    ("DaughterHouse", "91833325", "A wave consistensy challenge I made a longtime ago. The demon at the beginning is my sister.", ""),
+    ("SpaM I", "98669660", "First level from the SpaM series, it gave me a headache lol. Black orbs spam with cube", "-3mQ4wV0zag"),
+    ("Unnamed", "91831572", "Straightfly challenge from Element 111 RG but easier.", "wX3nusb0ZHw"),
+    ("SpaM VII", "117690307", "Loooooong swing straight fly.", ""),
+    ("SpaM VI", "117690302", "Loooooong wave spam.", "", {"Uranium": "DzTbL22toR8"}),
+    ("SpaM V", "98737726", "Spam on spider.", "yolTjBAHFOs"),
+    ("SpaM IV", "98737697", "Easy UFO spam but the last click is tricky.", "tESDqZi61A4"),
+    ("SpaM III", "98737614", "Short easy wave spam.", "CEmyGhWZXtU"),
+    ("SpaM II", "98737338", "Easy pink orbs spam on cube.", "Dz4hY_DQBa4")
+]
---
+++ players.py
@@ -0,0 +1,118 @@
+players = [
+    (
+        "Gild56", "@gild56gmd",
+        [
+            "Shitty Amethyst",
+            "Shitty Acheron",
+            "Shitty Kyouki",
+            "Zetsubo",
+            "Psychedelique",
+            "Obsolete",
+            "La Longue Attente",
+            "Shmyak",
+            "The Ghost",
+            "Deadly",
+            "A R K A D",
+            "Hell",
+            "Game",
+            "Machinas Error",
+            "Rainbow",
+            "MG"
+        ],
+        [
+            "Slaughterhouse Spam",
+            "12qwe2d1",
+            "RubRub",
+            "RI Wave x2",
+            "Wave Challenge Final",
+            "Spam VIII",
+            "Pushing On",
+            "LegendOfConsistensy",
+            "DaughterHouse",
+            "SpaM I",
+            "Unnamed",
+            "SpaM VII",
+            "SpaM VI",
+            "SpaM V",
+            "SpaM IV",
+            "SpaM III",
+            "SpaM II"
+        ]
+    ),
+    (
+        "MysticNull", "@mystic_nullgmd",
+        [
+            "Shitty Andromeda"
+        ],
+        []
+    ),
+    (
+        "kevino", "@qelzzu",
+        [
+            "Shitty Amethyst",
+            "Psychedelique"
+        ],
+        []
+    ),
+    (
+        "Uranium", "@UraniumGMD",
+        [
+            "Shitty Andromeda",
+            "Shitty Amethyst",
+            "Shitty Acheron",
+            "Shitty Kyouki",
+            "Zetsubo",
+            "Psychedelique",
+            "Obsolete",
+            "La Longue Attente",
+            "Shmyak",
+            "The Ghost",
+            "Deadly",
+            "A R K A D",
+            "Hell",
+            "Game",
+            "Machinas Error",
+            "Rainbow",
+            "MG"
+        ],
+        [
+            "LegendOfConsistensy",
+            "Pushing On",
+            "RI Wave x2",
+            "SpaM VI"
+        ]
+    ),
+    (
+        "Itwabosballss", "@Picklehi27GD",
+        [
+            "Shitty Andromeda"
+        ],
+        []
+    ),
+    (
+        "Panzer", "@TheRealPanzer",
+        [
+            "Shitty Andromeda"
+        ],
+        []
+    ),
+    (
+        "minhmnvnnet", "@minhmnvnnet",
+        [
+            "Shitty Andromeda"
+        ],
+        []
+    ),
+    (
+        "Loliquiem", "@loliquiemgd",
+        [
+            "Shitty Andromeda"
+        ],
+        []
+    ),
+    (
+        "Ylissen", "@Ylissen",
+        [],
+        []
+    )
+]

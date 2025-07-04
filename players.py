f5f6f838e92ee06f416b3697e1b750cf7e599754
Gild56 <gild56gmd@gmail.com>
2025-07-05 00:13:14 +0200

    Add .gitignore and some description chnages

--- players.py
+++ players.py
@@ -1,11 +1,11 @@
 players = [
-    ("Gild56", ["@gild56gmd", "🇺🇦", "", "The owner and the coder of this website and the creator of all these levels :)"], [], []),
-    ("MysticNull", ["@mystic_nullgmd", "🇺🇸", "", "A friend, YouTuber and demonslayer from Florida. He verified Shitty Andromeda on video without even knowing it xD"], [], []),
+    ("Gild56", ["@gild56gmd", "🇺🇦", "Gild56", "The owner and the coder of this website and the creator of all these levels :) Hardest: Acu on Android 60hz jump from CraZy in 1.5 year and 115k attempts (yea krazy)"], [], []),
+    ("MysticNull", ["@mystic_nullgmd", "🇺🇸", "", "A friend, YouTuber and demonslayer from Florida. He verified Shitty Andromeda on video without even knowing noone ever completed it on video yet xD. He has his own discord server and website with a list which inspiried this one ;)"], [], []),
     ("kevino", ["@qelzzu", "🇩🇴", "", "A friend, the verifier of Psychedelique and the first vicor of Shitty Amethyst."], [], []),
-    ("Uranium", ["@UraniumGMD", "🇦🇿", "", "A friend and the top 1 player of the list by farming all the levels, GG :D"], [], []),
+    ("Uranium", ["@UraniumGMD", "🇦🇿", "", "A friend and the top 1 player of the list after farming all the levels, GG! :D Hardest: Poltergeist"], [], []),
     ("Itwabosballss", ["@Picklehi27GD", "", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("Panzer", ["@TheRealPanzer", "", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("minhmnvnnet", ["@minhmnvnnet", "", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("Loliquiem", ["@loliquiemgd", "", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
-    ("Ylissen", ["@Ylissen", "🇲🇦", "", "A friend and the top 4 Morocco player on Global List (la dash orbe noire)"], [], [])
+    ("Ylissen", ["@Ylissen", "🇲🇦", "Ylissen", "A friend and the top 4 Morocco player on Global List (la dash orbe noire) Hardest: Hypersonic"], [], [])
 ]

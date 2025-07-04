c7707ba8b6884683a639aa83eb89e142b472518e
Gild56 <gild56gmd@gmail.com>
2025-07-04 20:27:07 +0200

    Unnamed moved down

--- challenges_list.py
+++ challenges_list.py
@@ -9,11 +9,11 @@ challenges_list_top = [
     ("RI Wave x2", "92957129", "Rainbow had an \"impossible\" version and this is the wave from that version but two times in a row.", "4qaz8HmjEiA", {"Uranium": "DzTbL22toR8"}),
     ("DaughterHouse", "91833325", "A wave consistensy challenge I made a longtime ago. The demon at the beginning is my sister.", ""),
     ("SpaM I", "98669660", "First level from the SpaM series, it gave me a headache lol. Black orbs spam with cube", "-3mQ4wV0zag"),
-    ("Unnamed", "91831572", "Straightfly challenge from Element 111 RG but easier.", "wX3nusb0ZHw"),
     ("SpaM VII", "117690307", "Loooooong swing straight fly.", ""),
     ("SpaM VI", "117690302", "Loooooong wave spam.", "", {"Uranium": "DzTbL22toR8"}),
     ("SpaM V", "98737726", "Spam on spider.", "yolTjBAHFOs"),
     ("SpaM IV", "98737697", "Easy UFO spam but the last click is tricky.", "tESDqZi61A4"),
     ("SpaM III", "98737614", "Short easy wave spam.", "CEmyGhWZXtU"),
+    ("Unnamed", "91831572", "Straightfly challenge from Element 111 RG but easier.", "wX3nusb0ZHw"),
     ("SpaM II", "98737338", "Easy pink orbs spam on cube.", "Dz4hY_DQBa4")
 ]

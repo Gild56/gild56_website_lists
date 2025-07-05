929083c8af893f77ff3a6a9ff5902651c2a536ef
Gild56 <gild56gmd@gmail.com>
2025-07-05 17:55:23 +0200

    kevino's hardest

--- players.py
+++ players.py
@@ -1,7 +1,7 @@
 players: list[tuple[str, list[str], list[str], list[str]]] = [
     ("Gild56", ["@gild56gmd", "UA", "Gild56", "The owner and the coder of this website and the creator of all these levels :) Hardest: Acu on Android 60hz jump from CraZy in 1.5 year and 115k attempts (yea krazy)"], [], []),
     ("MysticNull", ["@mystic_nullgmd", "US", "MysticNull", "A friend, YouTuber, and demon slayer from Florida. He verified Shitty Andromeda on video without even knowing that no one had ever completed it on video before, lol. He has his own Discord server and a website with a list that inspired this one ;) Hardest: Acu"], [], []),
-    ("kevino", ["@qelzzu", "DO", "00kev", "A friend, the verifier of Psychedelique and the first vicor of Shitty Amethyst."], [], []),
+    ("kevino", ["@qelzzu", "DO", "00kev", "A friend, the verifier of Psychedelique and the first vicor of Shitty Amethyst. Hardest: Nantendo"], [], []),
     ("Uranium", ["@UraniumGMD", "AZ", "", "A friend and the top 1 player of the list after farming all the levels, GG! :D Hardest: Poltergeist"], [], []),
     ("Itwabosballss", ["@Picklehi27GD", "Unknown", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("Panzer", ["@TheRealPanzer", "Unknown", "", "A random victor of Shitty Andromeda on YouTube."], [], []),

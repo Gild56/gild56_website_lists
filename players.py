45c8ba62725453989e64e4dd22b0b5118b0aa531
Gild56 <gild56gmd@gmail.com>
2025-08-21 16:52:42 +0200

    error update

--- players.py
+++ players.py
@@ -6,7 +6,7 @@ players: list[tuple[str, list[str], list[str], list[str]]] = [
     ("Itwabosballss", ["@Picklehi27GD", "Unknown", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("Panzer", ["@TheRealPanzer", "Unknown", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
     ("Minhmnvnnet", ["@minhmnvnnet", "VN", "", "A random victor of Shitty Andromeda on YouTube."], [], []),
-    ("Loliquiem", ["@loliquiemgd", "KO", "", "Challenge was created for his request."], [], []),
+    ("Loliquiem", ["@loliquiemgd", "KR", "", "Challenge was created for his request."], [], []),
     ("Ylissen", ["@Ylissen", "MA", "", "A friend and the top 5 (former top 4 xD) Morocco player on Global List (la dash orbe noire) Hardest: Hypersonic"], [], []),
     ("Kar0lek", ["@AuroraGD46", "PO", "", ""], [], []),
     ("Djo2x", ["@GDIlHHHlI", "CV", "", ""], [], [])

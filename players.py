10abec1971a1a0a49c077ad7e8d6853ee79bfdad
Gild56 <gild56gmd@gmail.com>
2025-11-02 16:48:26 +0100

    uranium top 1 again

--- players.py
+++ players.py
@@ -1,5 +1,6 @@
 players: list[tuple[str, list[str], list[str], list[str]]] = [
     ("Gild56", ["@gild56gmd", "UA", "Gild56", "The owner and the coder of this website and the creator of all these levels :) Hardest: Acu on Android 60hz jump from CraZy in 1.5 year and 115k attempts (yea krazy)"], [], []),
+    ("Toffelz", ["@toffelz", "FR", "", "A french player, playtester of Shitty Boobawamba. Hardest: Bloodbath / Aftermath"], [], []),
     ("MysticNull", ["@mystic_nullgmd", "US", "MysticNull", "A friend, YouTuber, and demon slayer from Florida. He verified Shitty Andromeda on video without even knowing that no one had ever completed it on video before, lol. He has his own Discord server and a website with a list that inspired this one ;) Hardest: Acu"], [], []),
     ("kevino", ["@qelzzu", "DO", "00kev", "A friend, the verifier of Psychedelique and the first vicor of Shitty Amethyst. Hardest: Future Funk"], [], []),
     ("Uranium", ["@UraniumGMD", "AZ", "", "A friend and the top 1 player of the list after farming all the levels, GG! :D Hardest: Auto Play Arena, Second hardest: Artificial Dreams"], [], []),
@@ -16,7 +17,6 @@ players: list[tuple[str, list[str], list[str], list[str]]] = [
     ("ReypSuperior", ["@reypsuperior", "Unknown", "", "A person probably"], [], []),
     ("Matsuba", ["@MatsubaRELLA", "Unknown", "", "A person probably"], [], []),
     ("Kyon717", ["@kyon717", "Unknown", "", "A person probably"], [], []),
-    ("Toffelz", ["@toffelz", "FR", "", "A french player, playtester of Shitty Boobawamba. Hardest: Bloodbath / Aftermath"], [], []),
     ("AuguXD", ["@wasuretalover", "CL", "", "A player from Chile. Hardest: Wasureta asa jump from Moment"], [], []),
     ("ItzLuis", ["@", "ES", "", "A player from Spain. (wPofpof)"], [], [])
 ]
